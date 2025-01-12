d53d95d6d2e0e4a06d3c4b4929033907ff099700
Yasin <Yasin@YASIN-SANCHEZ>
2025-01-12 23:07:12 +0300

    Proje dosyası ekle.

---
+++ ArabaYarisi/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ArabaYarisi
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox araba;
+        private System.Windows.Forms.PictureBox sagaraba;
+        private System.Windows.Forms.PictureBox solaraba;
+        private System.Windows.Forms.PictureBox yol;
+        private System.Windows.Forms.Label label_skor;
+        private System.Windows.Forms.Timer zamanlayici;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label_skor = new System.Windows.Forms.Label();
+            this.zamanlayici = new System.Windows.Forms.Timer(this.components);
+            this.araba = new System.Windows.Forms.PictureBox();
+            this.sagaraba = new System.Windows.Forms.PictureBox();
+            this.solaraba = new System.Windows.Forms.PictureBox();
+            this.yol = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.araba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sagaraba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.solaraba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yol)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_skor
+            // 
+            this.label_skor.AutoSize = true;
+            this.label_skor.Font = new System.Drawing.Font("Arial", 14F);
+            this.label_skor.Location = new System.Drawing.Point(10, 10);
+            this.label_skor.Name = "label_skor";
+            this.label_skor.Size = new System.Drawing.Size(107, 27);
+            this.label_skor.TabIndex = 4;
+            this.label_skor.Text = "SKOR: 0";
+            // 
+            // zamanlayici
+            // 
+            this.zamanlayici.Enabled = true;
+            this.zamanlayici.Interval = 20;
+            this.zamanlayici.Tick += new System.EventHandler(this.zamanlayici_Tick);
+            // 
+            // araba
+            // 
+            this.araba.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.araba.Image = global::ArabaYarisi.Properties.Resources.car;
+            this.araba.Location = new System.Drawing.Point(167, 389);
+            this.araba.Name = "araba";
+            this.araba.Size = new System.Drawing.Size(50, 100);
+            this.araba.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.araba.TabIndex = 0;
+            this.araba.TabStop = false;
+            // 
+            // sagaraba
+            // 
+            this.sagaraba.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.sagaraba.Image = global::ArabaYarisi.Properties.Resources.car_sag;
+            this.sagaraba.Location = new System.Drawing.Point(250, -100);
+            this.sagaraba.Name = "sagaraba";
+            this.sagaraba.Size = new System.Drawing.Size(50, 100);
+            this.sagaraba.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sagaraba.TabIndex = 1;
+            this.sagaraba.TabStop = false;
+            // 
+            // solaraba
+            // 
+            this.solaraba.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.solaraba.Image = global::ArabaYarisi.Properties.Resources.car_sol;
+            this.solaraba.Location = new System.Drawing.Point(50, -300);
+            this.solaraba.Name = "solaraba";
+            this.solaraba.Size = new System.Drawing.Size(50, 100);
+            this.solaraba.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.solaraba.TabIndex = 2;
+            this.solaraba.TabStop = false;
+            // 
+            // yol
+            // 
+            this.yol.BackColor = System.Drawing.Color.Gray;
+            this.yol.Image = global::ArabaYarisi.Properties.Resources.road;
+            this.yol.Location = new System.Drawing.Point(0, -621);
+            this.yol.Name = "yol";
+            this.yol.Size = new System.Drawing.Size(400, 1282);
+            this.yol.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.yol.TabIndex = 3;
+            this.yol.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(400, 600);
+            this.Controls.Add(this.araba);
+            this.Controls.Add(this.sagaraba);
+            this.Controls.Add(this.solaraba);
+            this.Controls.Add(this.yol);
+            this.Controls.Add(this.label_skor);
+            this.Name = "Form1";
+            this.Text = "Dikey Araba Yarışı";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.araba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sagaraba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.solaraba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yol)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ ArabaYarisi/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ArabaYarisi
+{
+    public partial class Form1 : Form
+    {
+        // Yolu aşağı doğru hareket ettiren hız değişkeni
+        int yolHizi = 5;
+
+        // Engellerin (arabaların) aşağı doğru hareket etme hızı
+        int engelHizi = 5;
+
+        // Oyuncunun topladığı skorları tutan değişken
+        int skor = 0;
+
+        // Formun yapıcı metodu (Constructor) - başlangıç ayarlarını yapar
+        public Form1()
+        {
+            InitializeComponent(); // Form bileşenlerini başlatır
+        }
+
+        // Zamanlayıcı her tetiklendiğinde çağrılan metot
+        private void zamanlayici_Tick(object sender, EventArgs e)
+        {
+            // Yolun aşağı doğru hareket etmesini sağlar
+            yol.Top += yolHizi;
+
+            // Sağdaki engelin (sagaraba) aşağı doğru hareket etmesini sağlar
+            sagaraba.Top += engelHizi;
+
+            // Soldaki engelin (solaraba) aşağı doğru hareket etmesini sağlar
+            solaraba.Top += engelHizi;
+
+            // Yolun tekrar yukarıdan görünmesini sağlar (sonsuz hareket efekti)
+            if (yol.Top > -72)
+            {
+                yol.Top = -200; // Yolun konumunu sıfırlar
+            }
+
+            // Sağdaki engel (sagaraba) ekranın dışına çıktığında yeniden yukarıdan başlar
+            if (sagaraba.Top > ClientSize.Height)
+            {
+                sagaraba.Top = -100; // Yeni başlangıç pozisyonu
+                sagaraba.Left = new Random().Next(100, ClientSize.Width - sagaraba.Width); // Rastgele yatay konum
+                skor++; // Skoru artırır
+            }
+
+            // Soldaki engel (solaraba) ekranın dışına çıktığında yeniden yukarıdan başlar
+            if (solaraba.Top > ClientSize.Height)
+            {
+                solaraba.Top = -100; // Yeni başlangıç pozisyonu
+                solaraba.Left = new Random().Next(100, ClientSize.Width - solaraba.Width); // Rastgele yatay konum
+                skor++; // Skoru artırır
+            }
+
+            // Skor etiketi güncellenir
+            label_skor.Text = "SKOR: " + skor;
+
+            // Çarpışma kontrolü: Oyuncunun arabası herhangi bir engelle çarpışırsa
+            if (araba.Bounds.IntersectsWith(sagaraba.Bounds) ||
+                araba.Bounds.IntersectsWith(solaraba.Bounds))
+            {
+                // Zamanlayıcı durdurulur
+                zamanlayici.Stop();
+
+                // Oyunun bittiği mesajı görüntülenir
+                MessageBox.Show("Oyun bitti! Skorunuz: " + skor);
+
+                // Uygulama yeniden başlatılır
+                Application.Restart();
+            }
+        }
+
+        // Klavyede bir tuşa basıldığında çağrılan metot
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Sol ok tuşuna basılırsa ve araç ekranın solunda değilse
+            if (e.KeyCode == Keys.Left && araba.Left > 0)
+            {
+                araba.Left -= 10; // Araç sola hareket eder
+            }
+
+            // Sağ ok tuşuna basılırsa ve araç ekranın sağında değilse
+            if (e.KeyCode == Keys.Right && araba.Right < ClientSize.Width)
+            {
+                araba.Left += 10; // Araç sağa hareket eder
+            }
+        }
+    }
+}
